ffa7ddff415d568e1aa4ec01e8d06f4d84ef85ea
xdwang0726 <starwxd@gmail.com>
2018-11-29 13:54:58 -0500

    Update eval_helper.py

--- eval_helper.py
+++ eval_helper.py
@@ -89,3 +89,71 @@ def ndcg_score(y_true, y_score, k, gains="linear"):
     best = dcg_score(y_true, y_true, k, gains)
     actual = dcg_score(y_true, y_score, k, gains)
     return actual / best
+
+def macro_precision(TP, FP):
+    MaP = []
+    for i in range(len(TP)):
+        macro_p = TP[i]/(TP[i] + FP[i])
+        MaP.append(macro_p)
+    MaP = np.mean(MaP)
+    return MaP
+
+def macro_recall(TP, FN):
+    MaR = []
+    for i in range(len(TP)):
+        macro_r = TP[i]/(TP[i] + FN[i])
+        MaR.append(macro_r)
+    MaR = np.mean(MaR)
+    return MaR
+
+def micro_precision(TP, FP):
+    MiP = sum(TP)/(sum(TP) + sum(FP))
+    return MiP
+
+def micro_recall(TP, FN):
+    MiR = sum(TP)/(sum(TP) + sum(FN))
+    return MiR
+
+def macro_f1(MaP, MaR):
+    MaF = stats.hmean([MaP, MaR])
+    return MaF
+
+def micro_f1(MiP, MiR):
+    MiF = stats.hmean([MiP, MiR])
+    return MiF
+
+def perf_measure(y_actual, y_hat):
+    result = []
+    TP_total = []
+    FP_total = []
+    TN_total = []
+    FN_total = []
+
+    for i in range(y_actual.shape[0]): 
+        TP = 0
+        FP = 0
+        TN = 0
+        FN = 0
+        for j in range(y_actual.shape[1]):
+            if y_actual[i,j]==y_hat[i,j]==1:
+                TP += 1
+            if y_hat[i,j]==1 and y_actual[i,j]!=y_hat[i,j]:
+                FP += 1
+            if y_actual[i,j]==y_hat[i,j]==0:
+                TN += 1
+            if y_hat[i,j]==0 and y_actual[i,j]!=y_hat[i,j]:
+                FN += 1         
+        TP_total.append(TP)
+        FP_total.append(FP)
+        TN_total.append(TN)
+        FN_total.append(FN)
+    
+    MaP = macro_precision(TP_total, FP_total)
+    MiP = micro_precision(TP_total, FP_total)
+    MaR = macro_recall(TP_total, FN_total)
+    MiR = micro_recall(TP_total, FN_total)
+    MaF = macro_f1(MaP, MaR)
+    MiF = micro_f1(MiP, MiR)
+    
+    result = [MaP, MiP, MaF, MiF]
+    return result
